c651e99773394aafa0ed569d6c1681224d96a2b9
Authur <1738238581@qq.com>
2020-03-08 06:32:55 +0800

    add recommend

--- src/pages/Home/components/Icons.vue
+++ src/pages/Home/components/Icons.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="icons">
-    <swiper>
+    <swiper :options="swiperIcons">
       <swiper-slide v-for="(page, index) of pages" :key="index">
         <div class="icon" v-for="item of page" :key="item.id">
           <div class="icon-img">
@@ -18,10 +18,13 @@ export default {
   name: 'HomeIcons',
   data () {
     return {
+      swiperIcons: {
+        autoplay: 0
+      },
       iconsList: [{
         id: '0001',
         imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
-        desc: '热门景点啊啊啊啊啊啊啊'
+        desc: '热门景点'
       }, {
         id: '0002',
         imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
@@ -84,33 +87,35 @@ export default {
   //   overflow hidden
   //   height 0
   //   padding-bottom 50%
-  .icon
-    position relative
-    overflow hidden
-    float left
-    width 25%
-    height 0
-    padding-bottom 25%
-    .icon-img
-      position absolute
-      top 0
-      left 0
-      right 0
-      bottom .44rem
-      box-sizing border-box
-      padding .1rem
-      .icon-img-content
-        display block
-        height 100%
-        margin 0 auto
-    .icon-desc
-      position absolute
-      left 0
-      right 0
-      bottom 0
-      height .44rem
-      line-height .44rem
-      text-align center
-      color $darkTextColor
-      ellipsis()
+  .icons
+    margin-top .2rem
+    .icon
+      position relative
+      overflow hidden
+      float left
+      width 25%
+      height 0
+      padding-bottom 25%
+      .icon-img
+        position absolute
+        top 0
+        left 0
+        right 0
+        bottom .44rem
+        box-sizing border-box
+        padding .1rem
+        .icon-img-content
+          display block
+          height 100%
+          margin 0 auto
+      .icon-desc
+        position absolute
+        left 0
+        right 0
+        bottom 0
+        height .44rem
+        line-height .44rem
+        text-align center
+        color $darkTextColor
+        ellipsis()
 </style>
---
+++ src/pages/Home/components/Recommend.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <!-- 因为宽和高都是定死的，所以这个外层包裹盒子就没用处 -->
+        <!-- <div class="item-img-wrapper"> -->
+          <img class="item-img" :src="item.imgUrl" />
+        <!-- </div> -->
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1912/62/6203253b6ea0d27ea3.img.jpg_200x200_327e7c03.jpg',
+        title: '美的鹭湖探索王国',
+        desc: '丰富有趣的奇妙世界，迷失在自己的王国'
+      }, {
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1912/62/6203253b6ea0d27ea3.img.jpg_200x200_327e7c03.jpg',
+        title: '美的鹭湖探索王国',
+        desc: '丰富有趣的奇妙世界，迷失在自己的王国'
+      }, {
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1912/62/6203253b6ea0d27ea3.img.jpg_200x200_327e7c03.jpg',
+        title: '美的鹭湖探索王国',
+        desc: '丰富有趣的奇妙世界，迷失在自己的王国'
+      }, {
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1912/62/6203253b6ea0d27ea3.img.jpg_200x200_327e7c03.jpg',
+        title: '美的鹭湖探索王国',
+        desc: '丰富有趣的奇妙世界，迷失在自己的王国'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top .2rem
+    line-height .4rem
+    background #eeeeee
+    text-indent .2rem
+  .item
+    overflow hidden
+    display flex
+    height 1.9rem
+    // background red
+    .item-img
+      width 1.7rem
+      height 1.7rem
+      padding .1rem
+    .item-info
+      flex 1
+      padding .1rem
+      min-width 0
+      .item-title
+        line-height .54rem
+        font-size .32rem
+        ellipsis()
+      .item-desc
+        line-height .4rem
+        ellipsis()
+        color #cccccc
+      .item-button
+        line-height .44rem
+        margin-top .16rem
+        background #ff9300
+        padding 0 .2rem
+        border-radius .06rem
+        color #ffffff
+</style>
